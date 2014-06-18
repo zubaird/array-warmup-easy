881b25400ef5cf6af31f7b615198727740e99af2
Jeff Taggart <jetaggart@users.noreply.github.com>
2014-06-17 21:18:33 -0600

    Update dogs.rb

--- dogs.rb
+++ dogs.rb
@@ -1,1 +1,33 @@
-DOGS = ["Fido", "Harleigh", "Mali", "Trixie", "Snow", "Victory"]
+dogs = ["Fido", "Harleigh", "Mali", "Trixie", "Snow", "Victory"]
+
+
+def name_lengths
+  "fill me in!"
+end
+
+def reverse_dog_names
+  "fill me in!"
+end
+
+def first_three_dogs_with_each
+  "fill me in!"
+end
+
+def first_three_dogs_without_each
+  "fill me in!"
+end
+
+def reverse_case_dog_names
+  "fill me in!"
+end
+
+def sum_of_all_dog_name_lengths
+  "fill me in!"
+end
+
+p name_lengths
+p reverse_dog_names
+p first_three_dogs_with_each
+p first_three_dogs_without_each
+p reverse_case_dog_names
+p sum_of_all_dog_name_lengths
