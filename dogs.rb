ccd09c0a16953fd71292f32882ca8101d0b938fa
Jeff Taggart <jetaggart@users.noreply.github.com>
2014-06-17 21:39:46 -0600

    Update dogs.rb

--- dogs.rb
+++ dogs.rb
@@ -2,30 +2,35 @@ dogs = ["Fido", "Harleigh", "Mali", "Trixie", "Snow", "Victory"]
 
 
 def name_lengths
-  "fill me in!"
+  []
 end
 
 def reverse_dog_names
-  "fill me in!"
+  []
 end
 
 def first_three_dogs_with_each
-  "fill me in!"
+  []
 end
 
 def first_three_dogs_without_each
-  "fill me in!"
+  []
 end
 
 def reverse_case_dog_names
-  "fill me in!"
+  []
 end
 
 def sum_of_all_dog_name_lengths
-  "fill me in!"
+  0
 end
 
-p name_lengths
+def dogs_with_long_names
+  []
+end
+
+p "Make each of these prints turn true"
+p name_lengths == []
 p reverse_dog_names
 p first_three_dogs_with_each
 p first_three_dogs_without_each
