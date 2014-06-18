a5339e0886e67c6d1fd298a6a4bdec9c4e2cd66a
Jeff Taggart <jeff@jetaggart.com>
2014-06-17 22:02:04 -0600

    formatting

--- dogs.rb
+++ dogs.rb
@@ -45,9 +45,9 @@ puts "Make each method return the correct value"
 puts "The check method will run and tell you if the answer is correct"
 puts "*"*80
 
-check("how_many_dogs", how_many_dogs(dogs) == 6 )
-check("name_lengths", name_lengths(dogs) == [4, 8, 4, 6, 4, 7] )
-check("reverse_dog_names", reverse_dog_names(dogs) == ["odiF", "hgielraH", "ilaM", "eixirT", "wonS", "yrotciV"] )
+check("how_many_dogs", how_many_dogs(dogs) == 6)
+check("name_lengths", name_lengths(dogs) == [4, 8, 4, 6, 4, 7])
+check("reverse_dog_names", reverse_dog_names(dogs) == ["odiF", "hgielraH", "ilaM", "eixirT", "wonS", "yrotciV"])
 check("first_three_dogs_with_each", first_three_dogs_with_each(dogs) == ["Fido", "Harleigh", "Mali"])
 check("first_three_dogs_without_each", first_three_dogs_without_each(dogs) == ["Fido", "Harleigh", "Mali"])
 check("reverse_case_dog_names", reverse_case_dog_names(dogs) == ["fIDO", "hARLEIGH", "mALI", "tRIXIE", "sNOW", "vICTORY"])
