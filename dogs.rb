f74e78fadcd634d1b78f4cd524a917319b79af03
zubaird <zubair.desai@gmail.com>
2014-10-29 15:11:31 -0700

    Complete Dogs.rb

--- dogs.rb
+++ dogs.rb
@@ -4,33 +4,60 @@ dogs = ["Fido", "Harleigh", "Mali", "Trixie", "Snow", "Victory"]
 
 def how_many_dogs(dogs)
 
+  dogs.size
+
 end
 
 def name_lengths(dogs)
+
+  dogs.map {|x| x.size }
 
 end
 
 def reverse_dog_names(dogs)
 
+  dogs.map { |x| x.reverse}
+
 end
 
 def first_three_dogs_with_each(dogs)
 
+  first_three = []
+
+  dogs.each do |dog|
+    first_three.push(dog) if first_three.size < 3
+  end
+
+  first_three
 end
 
 def first_three_dogs_without_each(dogs)
+
+  dogs[0..2]
 
 end
 
 def reverse_case_dog_names(dogs)
 
+  dogs.map { |x| x.swapcase }
+
 end
 
 def sum_of_all_dog_name_lengths(dogs)
 
+  dogs.join.size
+
 end
 
 def dogs_with_long_names(dogs)
+
+  dogs.map do |dog|
+    if dog.size > 5
+      dog = true
+    else
+      dog = false
+    end
+  end
 
 end
 
@@ -47,4 +74,3 @@ check("first_three_dogs_without_each", first_three_dogs_without_each(dogs) == ["
 check("reverse_case_dog_names", reverse_case_dog_names(dogs) == ["fIDO", "hARLEIGH", "mALI", "tRIXIE", "sNOW", "vICTORY"])
 check("sum_of_all_dog_name_lengths", sum_of_all_dog_name_lengths(dogs) == 33)
 check("dogs_with_long_names", dogs_with_long_names(dogs) == [false, true, false, true, false, true])
-
